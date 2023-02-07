9e067b0a5f0ac39184ce3bdc9de2ca66a99c7ea0
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2023-02-07 11:54:00 +0900

    fix run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 # I am adopting the original files and then resetting the git repo to apply my changes.
 # This is done to avoid issues when original files already exist on the machine and cannot be stowed.
 
-stowit zsh
-
-git reset --hard
-
 stowit() {
     stow -t ~ -R $1 --adopt
 }
+
+stowit zsh
+
+git reset --hard
