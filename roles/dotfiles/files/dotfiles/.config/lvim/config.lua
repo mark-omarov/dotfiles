e7596db41ed19c65e011a7c5b4bd8b9bdbbb3bff
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2024-10-10 06:44:18 +0900

    feat(lvim): copilot

--- roles/dotfiles/files/dotfiles/.config/lvim/config.lua
+++ roles/dotfiles/files/dotfiles/.config/lvim/config.lua
@@ -19,3 +19,15 @@ require('lvim.lsp.manager').setup('yamlls', {
     }
   }
 })
+
+table.insert(lvim.plugins, {
+  "zbirenbaum/copilot-cmp",
+  event = "InsertEnter",
+  dependencies = { "zbirenbaum/copilot.lua" },
+  config = function()
+    vim.defer_fn(function()
+      require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+      require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+    end, 100)
+  end,
+})
